6560ccae36ec2c200f0867f96cb5a662b720971d
keshavrcg <keshavkhandelwal.it.jecrc@gmail.com>
2016-12-26 13:39:57 -0800

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {  //Listen for events on server (only for connection or to emit to all we use io else we use socket)
   console.log('New user connected');
 
+  socket.emit('newMessage',{              //This will emit to only the client himself
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {     //This will emit to everyone except the broadcaster himself
+    from: 'Admin',
+    text: 'New User Connected',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {   //emit for emitter and on for listener so this will listen from client
     console.log('createMessage', message);
     io.emit('newMessage', {                   //This will emit to everyone
@@ -21,6 +33,11 @@ io.on('connection', (socket) => {  //Listen for events on server (only for conne
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect',()=> {
